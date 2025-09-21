d5a6f963faf117ecd20f9712b3e50ee88306a7e9
Abhal Benny <abhalbenny3@gmail.com>
2025-09-21 21:34:00 +0530

    j

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 // Create an Axios instance
 const api = axios.create({
   baseURL: "http://localhost:5000", // 🔹 your backend base URL
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
