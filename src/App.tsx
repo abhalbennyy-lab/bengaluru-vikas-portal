5de4e361fe68f6e0168a0572f18103acf86b7bc5
DeepakBlesson <deepakblessonrose14@gmail.com>
2025-09-21 15:35:27 +0530

    Gallery

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import BrandBengaluru from "./pages/BrandBengaluru";
 import EODB from "./pages/EODB";
@@ -10,42 +11,21 @@ import Contact from "./pages/Contact";
 import AboutUs from "./pages/AboutUs";
 import Ongoingproject from "./pages/Ongoingproject";
 import NotFound from "./pages/NotFound";
+import NewsList from "./pages/NewsList";
+import NewsDetail from "./pages/NewsDetail";
+
 import AdminLayout from "./admin/AdminLayout";
 import HeroAdmin from "./admin/HeroAdmin";
 import SubAdmins from "./admin/SubAdmins";
 import SuperAdminEmail from "./admin/SuperAdminEmail";
 import NewsAdmin from "./admin/NewsAdmin";
-import NewsList from "./pages/NewsList";
-import NewsDetail from "./pages/NewsDetail";
+import GalleryAdmin from "./admin/GalleryAdmin";
+
 import { ThemeProvider } from "./contexts/ThemeContext";
 import "./index.css";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/news" element={<NewsList />} />
-          <Route path="/news/:id" element={<NewsDetail />} />
-          <Route path="/brand-bengaluru" element={<BrandBengaluru />} />
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route path="banner" element={<HeroAdmin />} />
-            <Route path="sub-admins" element={<SubAdmins />} />
-            <Route path="super-email" element={<SuperAdminEmail />} />
-            <Route path="news" element={<NewsAdmin />} />
-          </Route>
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -55,13 +35,26 @@ function App() {
           <Sonner />
           <BrowserRouter>
             <Routes>
+              {/* Public Routes */}
               <Route path="/" element={<Index />} />
+              <Route path="/news" element={<NewsList />} />
+              <Route path="/news/:id" element={<NewsDetail />} />
               <Route path="/brand-bengaluru" element={<BrandBengaluru />} />
               <Route path="/eodb" element={<EODB />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/about-us" element={<AboutUs />} />
               <Route path="/ongoingproject" element={<Ongoingproject />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+              {/* Admin Routes */}
+              <Route path="/admin" element={<AdminLayout />}>
+                <Route path="banner" element={<HeroAdmin />} />
+                <Route path="sub-admins" element={<SubAdmins />} />
+                <Route path="super-email" element={<SuperAdminEmail />} />
+                <Route path="news" element={<NewsAdmin />} />
+                <Route path="gallery" element={<GalleryAdmin />} />
+              </Route>
+
+              {/* Catch-All */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
